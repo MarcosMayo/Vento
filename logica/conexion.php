cfc4e59f3ef80053c45231c07f44247cba946698
MarcosMayo <marcosalvarolife@gmail.com>
2025-04-19 20:22:13 -0600

    feat: creacion de carpeta logica y de la conexion.php

---
+++ logica/conexion.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$clave = "123456789";
+$bd = "vento";
+
+
+$conexion = mysqli_connect($host,$usuario,$clave,$bd);
+if($conexion){
+
+echo "CONEXION EXITOSA";
+}else{
+
+    echo "nose pudo conectar";
+}
+?>
