c0ffeae8624437b6b967e1f9da28ef5c31c43e0e
MarcosMayo <marcosalvarolife@gmail.com>
2025-04-28 15:39:59 -0600

    feat: actualización en clientes.php

--- vistas/clientes.php
+++ vistas/clientes.php
@@ -1,7 +1,6 @@
 <?php include("../logica/conexion.php"); ?>
 <?php include("../plantillas/header.php"); ?>
-<?php include("../plantillas/menuu.php"); ?>
-
+<?php include("../plantillas/menuu.php"); ?><!-- Contenido principal -->
 <main class="p-3">
     <div class="container-fluid">
 
@@ -16,8 +15,8 @@
         </div>
 
         <!-- Tabla de clientes -->
-        <div class="table-responsive">
-            <table class="table table-hover table-striped table-bordered" id="tablaClientes">
+        <div class="table-responsive">  
+            <table class="table table-hover table-striped table-bordered">
                 <thead class="table-primary">
                     <tr>
                         <th scope="col"><i class="bi bi-file-text me-2"></i>FOLIO</th>
@@ -25,42 +24,41 @@
                         <th scope="col"><i class="bi bi-person-vcard me-2"></i>Apellido</th>
                         <th scope="col"><i class="bi bi-telephone-fill me-2"></i>Teléfono</th>
                         <th scope="col"><i class="bi bi-envelope-fill me-2"></i>Email</th>
+                        <th scope="col"><i class="bi bi-calendar-check me-2"></i>Direccion</th>   
                         <th scope="col" class="text-center"><i class="bi bi-gear-fill me-2"></i>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php
+                <?php
                     $sql = $conexion->query("SELECT * FROM clientes");
                     while ($dato = $sql->fetch_object()) { ?>
                         <tr>
-                            <td><?php echo htmlspecialchars($dato->nombre); ?></td>
-                            <td><?php echo htmlspecialchars($dato->apellidos); ?></td>
-                            <td><?php echo htmlspecialchars($dato->telefono); ?></td>
-                            <td><?php echo htmlspecialchars($dato->email); ?></td>
-                            <td><?php echo htmlspecialchars($dato->fecha_registro); ?></td>
-                            <td><?php echo htmlspecialchars($dato->folio); ?></td>
+                            <td><?php echo $dato->folio; ?></td>
+                            <td><?php echo $dato->nombre; ?></td>
+                            <td><?php echo $dato->apellido_paterno; ?></td>
+                            <td><?php echo $dato->telefono; ?></td>
+                            <td><?php echo $dato->correo; ?></td>
+                            <td><?php echo $dato->direccion; ?></td>
                             <td class="text-center">
-                                <button class="btn btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#editarClienteModal<?= $dato->id ?>">
+                                <button class="btn btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#editarClienteModal<?= $dato->id_cliente ?>">
                                     <i class="bi bi-pencil-square"></i>
                                 </button>
+                                <form action="#" method="POST" style="display:inline;" onsubmit="return confirmar();">
+                                    <input type="hidden" name="id" value="<?= $dato->id_usu ?>">
                                     <button type="submit" class="btn btn-sm btn-danger">
                                         <i class="bi bi-trash"></i>
                                     </button>
                                 </form>
                             </td>
                         </tr>
-
-                        <?php include("../modales/modalEditarclient.php"); ?>
+                        <?php include("../modales/modaleditarclientes.php"); ?>
 
                     <?php } ?>
                 </tbody>
             </table>
         </div>
-
     </div>
 </main>
-
-
 
 <?php include("../modales/modalClientes.php"); ?>
 <?php include("../plantillas/footer.php"); ?>
